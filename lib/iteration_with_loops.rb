7117df45e79f3f6cfa22f6ca54f103389d8b61ff
patrick simmons <hadroncollides@gmail.com>
2021-01-21 19:24:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ outer_results = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  longest_string_element = ""
+  longest_string_element = 
   while element_index < src[row_index].count do
     if src[row_index][element_index].length < longest_string_element.length
       longest_string_element = src[row_index][element_index]
